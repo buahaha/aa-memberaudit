1722887f32c0f3194e66698d1a8c4289594dad0f
ErikKalkoken <erik.kalkoken@gmail.com>
2020-09-25 14:48:31 +0200

    Fixes for Django 3, add tools

--- memberaudit/auth_hooks.py
+++ memberaudit/auth_hooks.py
@@ -8,27 +8,28 @@ from . import urls, __title__
 
 class MemberauditMenuItem(MenuItemHook):
     """ This class ensures only authorized users will see the menu entry """
+
     def __init__(self):
         # setup menu entry for sidebar
         MenuItemHook.__init__(
             self,
             _(__title__),
-            'fa fa-check-circle fa-fw',
-            'memberaudit:index',
-            navactive=['memberaudit:index']
+            "fas fa-check-circle fa-fw",
+            "memberaudit:index",
+            navactive=["memberaudit:index"],
         )
 
     def render(self, request):
-        if request.user.has_perm('memberaudit.basic_access'):
+        if request.user.has_perm("memberaudit.basic_access"):
             return MenuItemHook.render(self, request)
-        return ''
+        return ""
 
 
-@hooks.register('menu_item_hook')
+@hooks.register("menu_item_hook")
 def register_menu():
     return MemberauditMenuItem()
 
 
-@hooks.register('url_hook')
+@hooks.register("url_hook")
 def register_urls():
-    return UrlHook(urls, 'memberaudit', r'^memberaudit/')
+    return UrlHook(urls, "memberaudit", r"^memberaudit/")
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from setuptools import find_packages, setup
 
 from memberaudit import __version__
@@ -6,33 +6,42 @@ from memberaudit import __version__
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='aa-memberaudit',
+    name="aa-memberaudit",
     version=__version__,
     packages=find_packages(),
     include_package_data=True,
-    license='MIT',
-    description='tbd.',
+    license="MIT",
+    description="tbd.",
     long_description=long_description,
-    long_description_content_type='text/markdown',    
-    author='John Doe',
-    author_email='john.doe@memberaudit.com',
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ErikKalkoken/aa-memberaudit",
+    author="Erik Kalkoken",
+    author_email="kaloken87@gmail.com",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',        
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ]
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    python_requires="~=3.6",
+    install_requires=[
+        "allianceauth>=2.7.3",
+    ],
 )
