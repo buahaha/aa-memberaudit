b9ddebec41547cac6f2914b706a6513577157c84
ErikKalkoken <erik.kalkoken@gmail.com>
2021-01-08 15:04:53 +0100

    Update utils

--- memberaudit/tests/test_utils.py
+++ memberaudit/tests/test_utils.py
@@ -5,6 +5,7 @@ from unittest.mock import Mock, patch
 import requests
 
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.http import HttpRequest
 from django.test import TestCase
 from django.utils import translation
@@ -31,10 +32,13 @@ from ..utils import (
     generate_invalid_pk,
     datetime_round_hour,
     humanize_value,
+    ObjectCacheMixin,
 )
 from ..utils import set_test_logger
 
-MODULE_PATH = "{}.utils".format(__package__.partition(".")[0])
+_this_package = __package__.partition(".")[0]
+MODULE_PATH = "{}.utils".format(_this_package)
+CURRENT_PATH = "{}.tests.test_utils".format(_this_package)
 logger = set_test_logger(MODULE_PATH, __file__)
 
 
@@ -380,3 +384,71 @@ class TestFormatIskValue(NoSocketsTestCase):
 
     def test_precision(self):
         self.assertEqual(humanize_value(12340000000, 1), "12.3b")
+
+
+fake_objects = dict()
+
+
+class FakeManager(ObjectCacheMixin):
+    def create(self, name):
+        pk = len(fake_objects) + 1
+        fake_objects[pk] = self.model(pk, name)
+        return fake_objects[pk]
+
+    def get(self, pk):
+        return fake_objects[pk]
+
+    def select_related(self, query):
+        return self
+
+    @property
+    def model(self):
+        return FakeModel
+
+
+class _FakeMeta:
+    def __init__(self, app_label, model_name) -> None:
+        self.app_label = app_label
+        self.model_name = model_name
+
+
+class FakeModel:
+    def __init__(self, pk, name) -> None:
+        self.pk = pk
+        self.name = name
+
+    _meta = _FakeMeta("dummy", "fakemodel")
+    objects = FakeManager()
+
+
+@patch(
+    CURRENT_PATH + ".FakeManager._fetch_object_for_cache",
+    wraps=FakeModel.objects._fetch_object_for_cache,
+)
+class TestObjectCacheMixin(TestCase):
+    def setUp(self) -> None:
+        self.obj = FakeModel.objects.create(name="My Fake Model")
+        cache.clear()
+
+    def test_get_cached_1(self, mock_fetch_object_for_cache):
+        """when cache is empty, load from DB"""
+        obj = FakeModel.objects.get_cached(pk=self.obj.pk)
+
+        self.assertEqual(obj.name, "My Fake Model")
+        self.assertEqual(mock_fetch_object_for_cache.call_count, 1)
+
+    def test_get_cached_2(self, mock_fetch_object_for_cache):
+        """when cache is not empty, load from cache"""
+        obj = FakeModel.objects.get_cached(pk=self.obj.pk)
+
+        obj = FakeModel.objects.get_cached(pk=self.obj.pk)
+
+        self.assertEqual(obj.name, "My Fake Model")
+        self.assertEqual(mock_fetch_object_for_cache.call_count, 1)
+
+    def test_get_cached_3(self, mock_fetch_object_for_cache):
+        """when cache is empty, load from DB"""
+        obj = FakeModel.objects.get_cached(pk=self.obj.pk, select_related="dummy")
+
+        self.assertEqual(obj.name, "My Fake Model")
+        self.assertEqual(mock_fetch_object_for_cache.call_count, 1)
